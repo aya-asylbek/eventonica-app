db0d8fbd6bb0992f3f614aa3c287f74fd1b6d595
aya-asylbek <aichurek0990@gmail.com>
2025-03-13 00:43:08 -0700

    Added .DS_Store in .giignore file

--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
-import express, { json } from 'express';
-import cors from 'cors';
+import express, { json } from 'express'; //Backend framework
+import cors from 'cors';//To allow frontend requests
 import { config } from 'dotenv';
 config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(json());
+app.use(cors()); //Enables CORS, which allows your frontend to make requests to your backend 
+app.use(json()); //middleware automatically parses incoming JSON request bodies.
 
-// home route
+//route for the home page
 app.get('/', (req, res) => {
   res.send('Hello from Eventonica Server!');
 });
-
+//This starts the server and listens on the specified port,
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
