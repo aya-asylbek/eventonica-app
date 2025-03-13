4ff56496f0d7276441ae88c60e9c7dc0542f746b
aya-asylbek <aichurek0990@gmail.com>
2025-03-13 10:48:14 -0700

    Added routes to get all events and by id from sample file and check in browser and postman

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 import express, { json } from 'express'; //Backend framework
 import cors from 'cors';//To allow frontend requests
 import { config } from 'dotenv';
-config();
+config();//When you call config(), it reads the .env file and adds all the variables defined in it to the process.env object. 
+
+import { EVENTS } from './events.js';//to get data locally from my sample file events.js and test in postman 
+
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,6 +17,26 @@ app.use(express.json()); //middleware automatically parses incoming JSON request
 app.get('/', (req, res) => {
   res.send('Hello from Eventonica Server!');
 });
+
+// Route to get all events locally with no postgres from my sample file events.js
+ app.get('/events', (req, res) => {
+ res.json(EVENTS);  // Send imported data as JSON response
+ });
+
+
+ //(get event by id -locally )
+ app.get('/event/:id', (req, res) => {
+    const eventId = parseInt(req.params.id, 10); // Convert the event ID to a number
+    const event = EVENTS.find(e => e.id === eventId); // Find the event by ID
+  
+    if (!event) {
+      return res.status(404).json({ message: "Event not found" }); // If no event is found, return a 404 error
+    }
+  
+    res.json(event); // If the event is found, return it as JSON
+  });
+
+  
 //This starts the server and listens on the specified port,
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
