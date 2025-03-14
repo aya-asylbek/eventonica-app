261f9403c59988b19eae5fd4fac37ace060ea43d
aya-asylbek <aichurek0990@gmail.com>
2025-03-14 12:58:37 -0700

    Add db.js for PostgreSQL connection

---
+++ server/db.js
@@ -0,0 +1,16 @@
+import pkg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,  // This will 'eventonica' from Postgres
+    port: process.env.DB_PORT,
+});
+
+export default pool;
+
