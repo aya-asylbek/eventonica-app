92c033c0b2ffec52a1902621b77f8ba77ffbd3ff
aya-asylbek <aichurek0990@gmail.com>
2025-03-17 01:42:28 -0700

    Added query to delete event

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ app.get('/api/events/:id', async (req, res) => {
   }
 });
 
-
+//post- add new one
 app.post('/api/events', async (req, res) => {
   const { name, date, category } = req.body;
   try {
@@ -83,6 +83,23 @@ app.put('/api/events', async (req, res) => {
   }
 });
  
+//delete my event
+app.delete('/api/events/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await pool.query('DELETE FROM events WHERE id = $1 RETURNING *', [id]);
+
+    if (result.rows.length > 0) {
+      res.json({ message: 'Event deleted successfully' });
+    } else {
+      res.status(404).json({ message: 'Event not found' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
 
   
 //This starts the server and listens on the specified port,
